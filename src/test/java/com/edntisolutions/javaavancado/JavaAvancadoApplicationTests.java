7b064a39378b0201adcb3f3bb3377135f9989a99
edneyRoldao <edneyroldao@gmail.com>
2022-06-29 21:30:31 -0300

    estrutura projeto criado

---
+++ src/test/java/com/edntisolutions/javaavancado/JavaAvancadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edntisolutions.javaavancado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAvancadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
